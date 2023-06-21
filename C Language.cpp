67d1553a6cbfa9dcc8351a681933fa042a340225
Dhruvi86 <134355929+Dhruvi86@users.noreply.github.com>
2023-06-21 22:45:04 +0530

    Add files via upload

---
+++ C Language.cpp
@@ -0,0 +1,22 @@
+/*
+int %d
+double 5if
+char %c,%s
+float %f
+*/
+#include<stdio.h>
+main(){
+
+        int num_1 = 10;
+        char name='a';
+        float num2=25.28;
+        double num3=56.89;
+        printf("Hello World \n");
+        printf("%d \n" ,num_1);
+        printf("%c \n",name);
+        printf("%f \n",num2);
+        printf("%if \n",num3);
+}
+    
+        
+
---
+++ c language2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("Enter your value : \n");
+	scanf("%d" ,&a);
+	printf("Enter your value : \n");
+	scanf("%d" ,&b);
+	printf("This is your addition : %d" ,a+b);
+}
